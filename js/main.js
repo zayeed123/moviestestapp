0ef9c9f5cf714beaa18653be8cad4e7c43f39c78
zayeed123 <94979254+zayeed123@users.noreply.github.com>
2022-10-09 09:16:08 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const searchInput = document.getElementById('sbar1');
+
+
+const getMovies = () => { 
+    const searchTerm = searchInput.value;
+    const con = document.getElementById("movie-list-con");
+    con.innerHTML= '';
+
+    fetch(`http://www.omdbapi.com/?apikey=f1bdf8cf&s=${searchTerm}`)
+    .then(data => {
+        return data.json();
+    })
+        .then(response => {
+                        
+            console.log(response);
+            
+
+            response.Search.forEach(res => {
+                let movieCard = document.createElement("div");
+                movieCard.className = "grid1"
+                movieCard.innerText = res.Title;
+                movieCard.style.backgroundImage = "url('"+res.Poster+"')";
+
+                con.appendChild(movieCard);
+            });
+
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+searchInput.addEventListener("change", getMovies);
+
